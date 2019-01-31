a3d0846654e37aa29a8160a1abc08c1a4756c53d
chad Nickel <nickel.chad.5@gmail.com>
2019-01-30 16:09:48 -0800

    This is my updated HelloWorld source code.

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -9,7 +9,7 @@ class HelloWorld {
     }
 
     sayHello(){
-        console.log('Hello world!' +' My name is ' + this.firstName + ' ' + this.lastName);
+        console.log('Hello world!' +' My name is ' + this.firstName + ' ' + this.lastName + '.');
     }
 }
 
