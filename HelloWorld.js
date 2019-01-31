6ca7a67e007eaea42c5524d6140f2e6327eb3715
chad Nickel <nickel.chad.5@gmail.com>
2019-01-30 16:10:39 -0800

    This is my updated HelloWorld source code.

--- HelloWorld.js
+++ HelloWorld.js
@@ -5,7 +5,7 @@ var HelloWorld = (function () {
         this.lastName = lastName;
     }
     HelloWorld.prototype.sayHello = function () {
-        console.log('Hello world!' + ' My name is ' + this.firstName + ' ' + this.lastName);
+        console.log('Hello world!' + ' My name is ' + this.firstName + ' ' + this.lastName + '.');
     };
     return HelloWorld;
 })();
